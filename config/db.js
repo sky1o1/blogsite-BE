5aca7efbf85c89f34dcfc87393a4141b7287be59
sky1o1 <aakash.dangol101@gmail.com>
2022-02-12 19:08:10 +0545

    env issue fixed

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
+  await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-  console.log(`Mongo DB connected : ${conn.connection.host} `);
 };
 
 module.exports = connectDB;
